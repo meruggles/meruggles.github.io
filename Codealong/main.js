05f80b603736bdf821edfe92c3b626b8b96d9b3b
meruggles <admin@MacBookAir.local>
2016-02-03 19:58:24 -0500

    Test commit
    Test commit

---
+++ Codealong/main.js
@@ -0,0 +1,16 @@
+function maxOfThree(num1, num2, num3){
+  if(num1 > num2 && num1 > num3){
+    return num1;
+  }else if (num2 < num && num2 > num1){
+    return num2;
+  }else{
+    return num3;
+  }
+}
+
+document.write(maxOfThree(30, 30 ,8));
+
+
+function isVowel(letter){
+  if (letter != "a" && letter !="e" && )
+}
